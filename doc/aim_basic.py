606196a0dc0832aa67853ed2d2d75448fc628494
Valentino Volonghi aka dialtone <valentino@adroll.com>
2008-11-20 15:56:41 -0800

    version bump for release, remove comments from an example

--- doc/aim_basic.py
+++ doc/aim_basic.py
@@ -1,22 +1,13 @@
 from authorize import aim as aim_api
-
 from pprint import pprint
 
 LOGIN = u"LOGIN"
 KEY = u"TRAN_KEY"
 
-aim = aim_api.Api(LOGIN, KEY)
+# Setup the aim Api object.
+aim = aim_api.Api(LOGIN, KEY, is_test=True)
 
-
-# x_login: up to 20 chars
-# x_tran_key: 16 chars
-# x_type: AUTH_CAPTURE (default), AUTH_ONLY, CAPTURE_ONLY, CREDIT, PRIOR_AUTH_CAPTURE, VOID
-# x_amount: up to 15 digits with a decimal point, no dollar symbol, all inclusive (tax, shipping etc)
-# x_card_num: between 13 and 16 digits, with x_type == CREDIT only the last 4 digits are required
-# x_exp_date: MMYY, MM/YY, MM-YY, MMYYYY, MM/YYYY, MM-YYYY
-# x_trans_id: only required for CREDIT, PRIOR_AUTH_CAPTURE, VOID
-# x_auth_code: authorization code of an original transaction not authorized on the payment gateway, 6 chars only for CAPTURE_ONLY
-
+# Create a transaction against a credit card
 result_dict = aim.transaction(
     amount=u"16.00",
     card_num=u"4111111111111111",
@@ -26,6 +17,7 @@ pprint(result_dict)
 
 trans_id = result_dict.trans_id
 
+# Credit back the transaction passing the transaction_id that they gave us.
 result_dict_2 = aim.transaction(
     type=aim_api.CREDIT,
     amount=u"16.00",
--- doc/async_example.py
+++ doc/async_example.py
@@ -8,16 +8,23 @@ from twisted.internet import reactor, defer
 def run():
     cim_api = cim.Api(u'LOGIN', u'TRANS_KEY', is_test=True, async=True)
 
+    # We create a profile for one of our users.
     tree = yield cim_api.create_profile(card_number=u"4111111111111111",
                                         expiration_date=u"2008-07",
                                         customer_id=u"testaccount5")
 
     pprint(tree)
+    
+    # Store the profile id somewhere so that we can later retrieve it.
+    # CIM doesn't have a listing or search functionality so you'll
+    # have to keep this somewhere safe and associated with the user.
     profile_id = tree.customer_profile_id
 
+    # Retrieve again the profile we just created using the profile_id
     resp = yield cim_api.get_profile(customer_profile_id=profile_id)
     pprint(resp)
-
+    
+    # And let's now try to create a transaction on that profile.
     resp = yield cim_api.create_profile_transaction(
         customer_profile_id=profile_id,
         customer_payment_profile_id=resp.profile.payment_profiles.customer_payment_profile_id,
@@ -25,6 +32,7 @@ def run():
     )
     pprint(resp)
 
+    # We did what we needed, we can remove the profile for this example.
     resp = yield cim_api.delete_profile(customer_profile_id=profile_id)
     pprint(resp)
     reactor.stop()
